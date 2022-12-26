eace38f756cfc255a833867976d6f234936ac23d
x04000 <adrianylua@gmail.com>
2022-12-26 01:47:26 +0100

    Alpha1

---
+++ RSC/script.js
@@ -0,0 +1,12 @@
+function main() {
+    console.log("a")
+}
+function about() {
+    console.log("a")
+}
+function download() {
+    console.log("a")
+}
+function patchnotes() {
+    console.log("a")
+}
